973915b0996496b665105bd595ee4867b82e7a23
fullofempt <super.shlykov11@yandex.ru>
2025-02-28 13:59:03 +0500

    init

--- src/app/page.js
+++ src/app/page.js
@@ -1,88 +1,66 @@
+"use client"
 import Image from "next/image";
+import { useState } from "react"
 
 export default function Home() {
+  const [input, setInput] = useState("")
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+    <div className="grid items-center justify-items-center min-h-screen gap-2 font-[family-name:var(--font-geist-sans)]">
+
+      <header className="flex w-max">
+        <div className="grid grid-flow-col m-5 space-x-2 rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#ADE8F4] dark:hover:bg-[#b8f1ff] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44">
+          <button className="px-5 rounded-xl bg-cyan-500 hover:bg-cyan-600">
+            Заявка
+          </button>
+          <button className="px-5 rounded-xl bg-cyan-500 hover:bg-cyan-600">
+            Об университете
+          </button>
+          <button className="px-5 rounded-xl bg-cyan-500 hover:bg-cyan-600">
+            Материалы
+          </button >
+          <button className="px-5 rounded-xl bg-cyan-500 hover:bg-cyan-600">
+            Проекты учеников
+          </button >
+          <input
+            className="px-5 rounded-xl text-zinc-100 bg-cyan-800 hover:bg-cyan-900"
+            type="text"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            placeholder="Поиск"
+          />
+        </div>
+      </header>
+
+      <main className="grid grid-cols-2 w-screen h-200 overflow-hidden p-0">
+      <div className="relative w-full h-full">
         <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
+          className="w-full h-200 object-cover"
+          src="/img/09.03.03.png"
+          alt="Left Image"
           width={180}
           height={38}
           priority
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+        <div className="absolute inset-0 backdrop-blur-sm"></div>
+      </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
+      <div className="relative w-full h-full">
+        <Image
+          className="w-full h-200 object-cover"
+          src="/img/09.03.02.png"
+          alt="Right Image"
+          width={180}
+          height={38}
+          priority
+        />
+        <div className="absolute inset-0 backdrop-blur-sm"></div>
+      </div>
+    </main>
+
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="https://pspu.ru/?ysclid=m7oi0pn5dt184634939"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -93,9 +71,10 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Go to nextjs.org →
+          Перейти на сайт университета →
         </a>
       </footer>
+
     </div>
   );
 }
