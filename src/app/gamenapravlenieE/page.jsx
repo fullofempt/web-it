9f1b0614db38c038ef29dd33c2f2223d933c629b
fullofempt <super.shlykov11@yandex.ru>
2025-04-21 12:39:40 +0500

    fix

--- src/app/gamenapravlenieE/page.jsx
+++ src/app/gamenapravlenieE/page.jsx
@@ -58,6 +58,7 @@ export default function Page() {
             alt="Программисты"
             layout="fill"
             objectFit="cover"
+            className=''
           />
         </div>
         <div className="absolute inset-0 backdrop-blur-sm"></div>
